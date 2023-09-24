faf7dcc760fc7fde560246172f019b79584f7f3b
kon-pas <konpas@mail.com>
2023-09-24 16:30:29 +0200

    feat!: turn scripts to funtions
    BREAKING CHANGE: Cannot run scripts by executing files, like so:
    `$ ./scripts/minify.js`
    or with node
    `$ node ./scripts/minify.js`

--- scripts/minify.js
+++ scripts/minify.js
@@ -9,31 +9,37 @@
 import { promises as fs } from 'node:fs'
 import { join } from 'node:path'
 
-try {
-  let Bundler = process.isBun ? Bun : await import('esbuild')
+export default async function (opts) {
+  try {
+    let Bundler = process.isBun ? Bun : await import('esbuild');
 
-  await Bundler.build({
-    entryPoints: [join('node_modules', 'nuejs-core', 'src', 'nue.js')],
-    format: 'esm',
-    bundle: true,
-    outdir: 'www',
-    minify: true
-  })
+    await Bundler.build({
+      entryPoints: [join('node_modules', 'nuejs-core', 'src', 'nue.js')],
+      format: 'esm',
+      bundle: true,
+      outdir: 'www',
+      minify: true,
+    });
 
-  console.log('Minified Nue to wwwl/nue.js with', process.isBun ? 'Bun' : 'ESBuild')
+    console.log(
+      'Minified Nue to wwwl/nue.js with',
+      process.isBun ? 'Bun' : 'ESBuild'
+    );
+  } catch (e) {
+    console.log(
+      'No builder found (esbuild or Bun)',
+      'www/nue.js is not minified'
+    );
+    await copyNue();
+  }
 
-} catch (e) {
-  console.log('No builder found (esbuild or Bun)', 'www/nue.js is not minified')
-  await copyNue()
-}
-
-
-// no bundler -> just copy
-async function copyNue() {
-  const dir = join('node_modules', 'nuejs-core', 'src')
-  const files = await fs.readdir(dir)
-  for (const name of files) {
-    await fs.copyFile(join(dir, name), join('www', name))
+  // no bundler -> just copy
+  async function copyNue() {
+    const dir = join('node_modules', 'nuejs-core', 'src')
+    const files = await fs.readdir(dir)
+    for (const name of files) {
+      await fs.copyFile(join(dir, name), join('www', name))
+    }
   }
 }
 
