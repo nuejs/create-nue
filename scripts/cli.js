8636185de6c4b554bad297cbe5e941d0e1e01cd8
Tero Piirainen <tipiirai@gmail.com>
2023-09-25 13:24:26 +0300

    Removed comments

--- scripts/cli.js
+++ scripts/cli.js
@@ -1,16 +1,10 @@
 #!/usr/bin/env node
 
-/* NOTE:
-  The code here is a placeholder.
-  I just wanted to get it working as simple as possible.
-  The implementation may vary depending on the design. It is a topic to discuss.
-*/
-
 import { DEFAULT_OPTS } from './create.js'
 
 const argv = process.isBun ? Bun.argv : process.argv
 const script = argv[2]
-const opts = { ...DEFAULT_OPTS, ...{ prod: argv[3] == '--prod' } } // TODO: Implement parsing opts
+const opts = { ...DEFAULT_OPTS, ...{ prod: argv[3] == '--prod' } }
 
 try {
   (await import(`./${script}.js`)).default(opts)
